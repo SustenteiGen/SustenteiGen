b71e66647e928827e4213a20253a25f49eee1c3c
talitarribeiro <talitaribeiro.ribeiro03@hotmail.com>
2021-11-09 10:40:39 -0300

    Tabela Categoria

---
+++ BACKEND/sustentei/src/main/java/br/org/generation/sustentei/repository/CategoriaRepository.java
@@ -0,0 +1,16 @@
+package br.org.generation.sustentei.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.org.generation.sustentei.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository <Categoria, Long>{
+	public List <Categoria> findAllByTipoContainingIgnoreCase(String categoria);
+	
+	public List <Categoria> findAllByPalavraChaveContainingIgnoreCase(String palavraChave);	
+		
+}
