fa98469434836baf13c3f5c3cbd39bc80c8574d5
mcsnsx <mcs_nasc@hotmail.com>
2021-12-06 11:39:07 -0300

    Add models

---
+++ Frontend/src/models/Categoria.ts
@@ -0,0 +1,8 @@
+interface Categoria{
+    id: number;
+    descricao: string;
+    tipo: string;
+    palavraChave: string
+}
+
+export default Categoria;
--- Frontend/src/paginas/cadastro/CadastroUsuario.tsx
+++ Frontend/src/paginas/cadastro/CadastroUsuario.tsx
@@ -1,15 +1,86 @@
-import React from "react";
+import React , {useState, useEffect, ChangeEvent } from 'react';
+import { useHistory } from 'react-router-dom';
+import User from '../../models/User';
+import { cadastroUsuario } from '../../services/Service';
 import { Grid, Box, Typography, Button, TextField } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import './CadastroUsuario.css';
+import { toast } from 'react-toastify';
 
 function CadastroUsuario() {
+
+    let history = useHistory();
+    const [confirmarSenha,setConfirmarSenha] = useState<String>("")
+    const [user, setUser] = useState<User>(
+        {
+            id: 0,
+            nome: '',
+            usuario: '',
+            senha: ''
+        })
+
+    const [userResult, setUserResult] = useState<User>(
+        {
+            id: 0,
+            nome: '',
+            usuario: '',
+            senha: ''
+        })
+
+    useEffect(() => {
+        if (userResult.id != 0) {
+            history.push("/login")
+        }
+    }, [userResult])
+
+
+    function confirmarSenhaHandle(e: ChangeEvent<HTMLInputElement>){
+        setConfirmarSenha(e.target.value)
+    }
+
+
+    function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+
+    }
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault()
+        if(confirmarSenha == user.senha){
+        cadastroUsuario(`/usuarios/cadastrar`, user, setUserResult)
+        toast.success('Usuario cadastrado com sucesso', {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: false,
+            theme: "colored",
+            progress: undefined,
+            });
+        }else{
+            toast.error('Dados inconsistentes. Favor verificar as informações de cadastro.', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                theme: "colored",
+                progress: undefined,
+                });
+        }
+    }
+
     return (
         <Grid container direction='row' justifyContent='center' alignItems='center'>
             <Grid item xs={6} className='imagem2' ></Grid>
             <Grid item xs={6} alignItems='center'>
                 <Box paddingX={10}>
-                    <form>
+                    <form onSubmit={onSubmit}>
 
                         <Typography variant='h3' gutterBottom color='textPrimary' align='center' className='textos2'> Cadastrar </Typography>
                         <TextField id='nome' label='nome' variant='outlined' name='nome' margin='normal' fullWidth />
